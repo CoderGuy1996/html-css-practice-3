62544abed2fb4fe5ef27b9ed135993fc590209da
CoderGuy1996 <140737564+CoderGuy1996@users.noreply.github.com>
2023-12-05 15:07:28 +0530

    Add files via upload

---
+++ parallexWebsite/Parallex.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import './Parallex.css';
+function Parallex() {
+  return (
+    <body>
+        <section className='pimg1'>
+            <div className="ptext">
+                <span className='textBg'>Parallex Website</span>
+            </div>
+        </section>
+        <section className='section section-light'>
+            <h2>Section One</h2>
+            <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam aut
+            amet odio est laborum obcaecati perferendis! Mollitia officiis,
+            consectetur vitae laborum eos expedita reprehenderit nostrum tempore.
+            Atque laborum repudiandae adipisci nihil. Natus cum sapiente culpa
+            excepturi quasi magni quibusdam ipsum accusamus laborum, numquam odit!
+            Nisi repudiandae cumque incidunt tempora explicabo vero in corporis
+            provident quidem quisquam sit, asperiores ullam placeat ut iure pariatur
+            ratione laboriosam totam amet minus. Porro, quam quisquam explicabo
+            rerum labore, tempora officia eveniet mollitia iure a fugiat, magnam at.
+            Vero magni aliquam ea molestias omnis, quia fuga architecto maiores ut
+            magnam! Culpa, cum. Blanditiis, tempore temporibus.
+            </p>
+        </section>
+        <section className='pimg2'>
+            <div className="ptext">
+            <span class="textBg"> Explore Nature</span>
+            </div>
+        </section>
+        <section className='section section-dark'>
+            <h2>Section Tow</h2>
+            <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam aut
+            amet odio est laborum obcaecati perferendis! Mollitia officiis,
+            consectetur vitae laborum eos expedita reprehenderit nostrum tempore.
+            Atque laborum repudiandae adipisci nihil. Natus cum sapiente culpa
+            excepturi quasi magni quibusdam ipsum accusamus laborum, numquam odit!
+            Nisi repudiandae cumque incidunt tempora explicabo vero in corporis
+            provident quidem quisquam sit, asperiores ullam placeat ut iure pariatur
+            ratione laboriosam totam amet minus. Porro, quam quisquam explicabo
+            rerum labore, tempora officia eveniet mollitia iure a fugiat, magnam at.
+            Vero magni aliquam ea molestias omnis, quia fuga architecto maiores ut
+            magnam! Culpa, cum. Blanditiis, tempore temporibus.
+            </p>
+        </section>
+        <section className='pigm3'>
+            <div className="ptext">
+                <span className='textBg'>Look Deep Into Nature</span>
+            </div>
+        </section>
+        <section class="section section-dark">
+          <h2>Section Three</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam aut
+            amet odio est laborum obcaecati perferendis! Mollitia officiis,
+            consectetur vitae laborum eos expedita reprehenderit nostrum tempore.
+            Atque laborum repudiandae adipisci nihil. Natus cum sapiente culpa
+            excepturi quasi magni quibusdam ipsum accusamus laborum, numquam odit!
+            Nisi repudiandae cumque incidunt tempora explicabo vero in corporis
+            provident quidem quisquam sit, asperiores ullam placeat ut iure pariatur
+            ratione laboriosam totam amet minus. Porro, quam quisquam explicabo
+            rerum labore, tempora officia eveniet mollitia iure a fugiat, magnam at.
+            Vero magni aliquam ea molestias omnis, quia fuga architecto maiores ut
+            magnam! Culpa, cum. Blanditiis, tempore temporibus.
+          </p>
+        </section>  
+        <section class="pimg1">
+         <div class="ptext">
+           <span class="textBg"> Do Some Coding </span>
+         </div>
+        </section>
+    </body>
+  )
+}
+
+export default Parallex
